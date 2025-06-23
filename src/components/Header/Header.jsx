b2a8c51448fbb3f1941e5774af6c1158b197c0ef
Shriharsh Nandigamwar <nandigamwarharsh@gmail.com>
2025-06-23 10:35:52 +0530

    Bug fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <header className="flex items-center justify-between p-[12px] bg-[#1a1a1a] h-[50px] sticky w-full top-0">
+    <header className="flex items-center justify-between p-[12px] bg-[#1a1a1a] h-[50px] sticky w-full z-20 top-0">
       <div className="text-cyan-300 font-bold text-xl">{"< Shriharsh />"}</div>
       <div onClick={() => setShowMenu(!showMenu)}>
         <img
@@ -16,39 +16,52 @@ const Header = () => {
       </div>
       <div
         style={{ display: showMenu ? "flex" : "none" }}
-        className=" h-[200px] w-[150px] bg-[#1a1a1a] mt-[265px] flex flex-col items-center rounded-2xl p-[20px]  "
+        className=" h-[300px] w-[220px] bg-[#1a1a1a] mt-[370px] flex flex-col items-center rounded-2xl p-[20px] "
       >
         <ul>
-          <li
-            className="bg-[#1e1e1e] w-[100px] flex items-center justify-center rounded-[5px] hover:text-cyan-300 mb-2.5 cursor-pointer border border-cyan-300 "
-            onClick={() => setShowMenu(false)}
-          >
-            <a href="#home">Home</a>
-          </li>
-          <li
-            className="bg-[#1e1e1e] w-[100px] flex items-center justify-center rounded-[5px] hover:text-cyan-300 mb-2.5 cursor-pointer border border-cyan-300"
-            onClick={() => setShowMenu(false)}
-          >
-            <a href="#about">About</a>
-          </li>
-          <li
-            className="bg-[#1e1e1e] w-[100px] flex items-center justify-center rounded-[5px] hover:text-cyan-300 mb-2.5 cursor-pointer border border-cyan-300"
-            onClick={() => setShowMenu(false)}
-          >
-            <a href="#skills">Skills</a>
-          </li>
-          <li
-            className="bg-[#1e1e1e] w-[100px] flex items-center justify-center rounded-[5px] hover:text-cyan-300 mb-2.5 cursor-pointer border border-cyan-300"
-            onClick={() => setShowMenu(false)}
-          >
-            <a href="#projects">Projects</a>
-          </li>
-          <li
-            className="bg-[#1e1e1e] w-[100px] flex items-center justify-center rounded-[5px] hover:text-cyan-300 mb-2.5 cursor-pointer border border-cyan-300"
-            onClick={() => setShowMenu(false)}
-          >
-            <a href="#contact">Contact</a>
-          </li>
+          <a href="#home">
+            <li
+              className="bg-[#1e1e1e] w-[170px] h-[40px] flex items-center justify-center rounded-[5px] hover:text-cyan-300 mb-3.5  cursor-pointer border border-cyan-300 "
+              onClick={() => setShowMenu(false)}
+            >
+              Home
+            </li>
+          </a>
+
+          <a href="#about">
+            <li
+              className="bg-[#1e1e1e] w-[170px] h-[40px] flex items-center justify-center rounded-[5px] hover:text-cyan-300 mb-3.5  cursor-pointer border border-cyan-300"
+              onClick={() => setShowMenu(false)}
+            >
+              About
+            </li>
+          </a>
+
+          <a href="#skills">
+            <li
+              className="bg-[#1e1e1e] w-[170px] h-[40px] flex items-center justify-center rounded-[5px] hover:text-cyan-300 mb-3.5  cursor-pointer border border-cyan-300"
+              onClick={() => setShowMenu(false)}
+            >
+              Skills
+            </li>
+          </a>
+
+          <a href="#projects">
+            <li
+              className="bg-[#1e1e1e] w-[170px] h-[40px] flex items-center justify-center rounded-[5px] hover:text-cyan-300 mb-3.5  cursor-pointer border border-cyan-300"
+              onClick={() => setShowMenu(false)}
+            >
+              Projects
+            </li>
+          </a>
+          <a href="#contact">
+            <li
+              className="bg-[#1e1e1e] w-[170px] h-[40px] flex items-center justify-center rounded-[5px] hover:text-cyan-300 mb-3.5  cursor-pointer border border-cyan-300"
+              onClick={() => setShowMenu(false)}
+            >
+              Contact
+            </li>
+          </a>
         </ul>
       </div>
     </header>
