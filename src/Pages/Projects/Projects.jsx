d76e83f9dc9bbe58c9f9e5b0a54d4421b05edbfc
Shriharsh Nandigamwar <nandigamwarharsh@gmail.com>
2025-07-13 14:53:42 +0530

    code delete

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -14,49 +14,6 @@ const Projects = () => {
       </h1>
 
       <div className="projectDiv ">
-       
-
-       
-
-        {/* <div className="bg-[#1a1a1a] p-[15px] rounded-2xl flex flex-col items-center mt-[20px] mb-[20px] h-[500px] lg:w-[320px] ">
-          <div>
-            <img
-              src={weather}
-              alt="N/A"
-              className="h-[170px] w-[320px] rounded-xl "
-            />
-          </div>
-          <div>
-            <h2 className="flex justify-center mt-[15px] text-3xl text-cyan-300">
-              Weather app
-            </h2>
-            <p className="flex justify-center items-center font-[13px] mt-[10px]">
-              Check the weather forecast anytime anywhere. Get accurate and
-              up-to-date weather updates for your location.
-            </p>
-          </div>
-          <div className="flex flex-col justify-center items-center mt-[20px]">
-            <a
-              href="https://github.com/HarshNandigamwar/Weather-App"
-              target="_blank"
-            >
-              <img
-                src="https://skillicons.dev/icons?i=github"
-                alt="Github"
-                className="githubImg mb-[10px] h-[50px]"
-              />
-            </a>
-            <a
-              href="https://weather-app-shriharsh.netlify.app/"
-              target="_blank"
-            >
-              <button className=" p-[0.3rem] rounded-[8px] bg-white text-[1rem] border-[2px] border-cyan-300 text-black">
-                Live Preview
-              </button>
-            </a>
-          </div>
-        </div> */}
-
         <ProjectCard
           img={Sigma}
           projectName="Sigma AI"
@@ -92,42 +49,6 @@ const Projects = () => {
           liveLink="https://sigmamart.netlify.app/"
           gitLink="https://github.com/HarshNandigamwar/Sigma-mart"
         />
-
-        {/* <div className="bg-[#1a1a1a] p-[15px] rounded-2xl flex flex-col items-center mt-[20px] mb-[20px] h-[500px] lg:w-[320px] ">
-          <div>
-            <img
-              src={sigmaMart}
-              alt="N/A"
-              className="h-[170px] w-[320px] rounded-xl "
-            />
-          </div>
-          <div>
-            <h2 className="flex justify-center mt-[15px] text-3xl text-cyan-300">
-              E-Commerce app
-            </h2>
-            <p className="flex justify-center items-center font-[13px] mt-[10px]">
-              This is a dynamic E-Commerce web application built with React.js,
-              designed to deliver a smooth and engaging shopping experience.
-            </p>
-          </div>
-          <div className="flex flex-col justify-center items-center mt-[13px]">
-            <a
-              href="https://github.com/HarshNandigamwar/Sigma-mart"
-              target="_blank"
-            >
-              <img
-                src="https://skillicons.dev/icons?i=github"
-                alt="Github"
-                className="githubImg mb-[10px] h-[50px]"
-              />
-            </a>
-            <a href="https://sigmamart.netlify.app/" target="_blank">
-              <button className=" p-[0.3rem] rounded-[8px] bg-white text-[1rem] border-[2px] border-cyan-300 text-black">
-                Live Preview
-              </button>
-            </a>
-          </div>
-        </div> */}
       </div>
     </div>
   );
